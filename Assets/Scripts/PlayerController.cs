1dc523326a3c8d42b8858587994d640ade9ad19f
JleseelleurQUB <josephleseelleur@gmail.com>
2023-11-19 16:09:49 +0000

    6.2.3
    - Added UI elements to track vehicle speed and RPM
    
    - (independent) Improved vehicle handling by implementing a maximum speed and manually implementing decelerative forces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float horsePower = 100;
+    [SerializeField] float speed;
     [SerializeField] float turnSpeed = 40.0f;
+    [SerializeField] float rpm;
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody rb;
     [SerializeField] GameObject centreOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
 
     private void Start()
     {
@@ -20,11 +25,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        // Update the speedometer and rpm
+        speed = Mathf.RoundToInt(rb.velocity.magnitude * 3.6f);
+        speedometerText.SetText("Speed: " + speed + "km/h");
+        rpm = Mathf.RoundToInt((speed % 30) * 40);
+        rpmText.SetText("RPM: " + rpm);
+
+
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward based on vertical input
-        rb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+        if (speed < 100)
+        {
+            rb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+        }
+
         // Rotate the vehicle based on horizontal input
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        // Slow the vehicle when player is no longer pressing forward
+        if(horizontalInput == 0 && speed > 0)
+        {
+            rb.AddRelativeForce(Vector3.forward * -4000);
+        }
+
     }
 }
