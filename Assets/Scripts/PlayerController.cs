99087ec8863fdef7d2983237d443a372119b6672
JleseelleurQUB <josephleseelleur@gmail.com>
2023-11-19 16:39:44 +0000

    6.2.4
    - Added bool function to check for vehicle being fully grounded in order to be able to drive

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,9 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody rb;
+    private int wheelsOnGround;
     [SerializeField] GameObject centreOfMass;
+    [SerializeField] List<WheelCollider> allWheels;
     [SerializeField] TextMeshProUGUI speedometerText;
     [SerializeField] TextMeshProUGUI rpmText;
 
@@ -26,30 +28,53 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
 
-        // Update the speedometer and rpm
-        speed = Mathf.RoundToInt(rb.velocity.magnitude * 3.6f);
-        speedometerText.SetText("Speed: " + speed + "km/h");
-        rpm = Mathf.RoundToInt((speed % 30) * 40);
-        rpmText.SetText("RPM: " + rpm);
-
-
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        // Move the vehicle forward based on vertical input
-        if (speed < 100)
+
+        if (IsOnGround())
         {
-            rb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+            // Update the speedometer and rpm
+            speed = Mathf.RoundToInt(rb.velocity.magnitude * 3.6f);
+            speedometerText.SetText("Speed: " + speed + "km/h");
+            rpm = Mathf.RoundToInt((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+
+
+            // Move the vehicle forward based on vertical input
+            if (speed < 100)
+            {
+                rb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+            }
+
+            // Rotate the vehicle based on horizontal input
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+            // Slow the vehicle when player is no longer pressing forward
+            if (horizontalInput == 0 && speed > 0)
+            {
+                rb.AddRelativeForce(Vector3.forward * -4000);
+            }
         }
+    }
 
-        // Rotate the vehicle based on horizontal input
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
-
-        // Slow the vehicle when player is no longer pressing forward
-        if(horizontalInput == 0 && speed > 0)
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach(WheelCollider wheel in allWheels)
         {
-            rb.AddRelativeForce(Vector3.forward * -4000);
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
         }
-
+        if(wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
